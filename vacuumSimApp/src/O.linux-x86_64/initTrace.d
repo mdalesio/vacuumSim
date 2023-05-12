d459d3e48b14fd38a6558213182cbeb0a99d159a
mdalesio <michael.dalesio@gmail.com>
2023-05-12 11:39:29 -0400

    Removed make products

--- vacuumSimApp/src/O.linux-x86_64/initTrace.d
+++
@@ -1,8 +0,0 @@
-initTrace.o: ../initTrace.c \
- ../../../../build-epics/epics-base/include/initHooks.h \
- ../../../../build-epics/epics-base/include/libComAPI.h \
- ../../../../build-epics/epics-base/include/epicsExport.h \
- ../../../../build-epics/epics-base/include/shareLib.h \
- ../../../../build-epics/epics-base/include/iocsh.h \
- ../../../../build-epics/epics-base/include/compilerDependencies.h \
- ../../../../build-epics/epics-base/include/compiler/gcc/compilerSpecific.h
